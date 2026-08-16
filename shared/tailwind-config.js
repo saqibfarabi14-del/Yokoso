3fcf0d47dc5e7cf8608920a44df9541249987d48
saqibfarabi14-del <saqibfarabi14@gmail.com>
2026-08-16 16:58:23 +0600

    Extract shared files: add shared/tailwind-config.js

---
+++ shared/tailwind-config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                bone: '#F7F2E8',
+                'bone-dark': '#ede6d8',
+                forest: '#16302B',
+                'forest-light': '#2a4f47',
+                vermilion: '#D43A2F',
+                'vermilion-light': '#e85a4f',
+            },
+            fontFamily: {
+                script: ['"Great Vibes"', 'cursive'],
+                serif: ['"Playfair Display"', 'serif'],
+                sans: ['"Inter"', 'sans-serif'],
+            },
+        },
+    },
+};
